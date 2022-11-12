e3a949b6678d9ec69d626db1c2e096387f49e359
craighughes58 <90798245+craighughes58@users.noreply.github.com>
2022-11-12 17:44:43 -0600

    comments 3

--- Light_Maze/Light Maze/Assets/Scripts/PlayerController.cs
+++ Light_Maze/Light Maze/Assets/Scripts/PlayerController.cs
@@ -14,54 +14,57 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    //
+    //check if the player has already clicked any of the move buttons
     private bool HasMoved;
     //0 = up, 1 = down, 2 = left, 3 = right
     private int direction;
-    [Tooltip("")]
+    [Tooltip("How fast the player moves")]
     public float speed;
-    //
+    //the reference to the player's rigidbody2d
     private Rigidbody2D rb2d;
-    [Tooltip("")]
+    [Tooltip("The object made behind the player")]
     public GameObject Line;
-    //
+    //the reference to the gamecontroller
     private GameController GameCon;
-    //
+    //the reference to the newest line's collider
     private Collider2D NextCollide;
-    //
+    //the point that the last line ends
     private Vector2 LastLineEnd;
-    //
+    //bool that keeps track of if the player has won or not
     private bool won = false;
-    [Tooltip("")]
+    [Tooltip("The name of the current scene")]
     public string CurrentScene;
-    [Tooltip("")]
+    [Tooltip("The speed when the player is slowing down")]
     public float speedControl;
-    [Tooltip("")]
+    [Tooltip("is a tutorial level")]
     public bool tutorial;
-    //
+    //represents if the speed needs to be slowed or not true = yes, false = no
     private bool breaks;
-    //
+    //the maximum amount of time the player can use the break
     private float breakTimer;
-    //
+    // a reference to the UI that displays how much time on the break timer is left
     private GameObject BreakSlider;
-    [Tooltip("")]
+    [Tooltip("The sound made when the player changes direction")]
     public AudioClip Turn;
-    [Tooltip("")]
+    [Tooltip("The sound made when the player wins")]
     public AudioClip WinSound;
-    [Tooltip("")]
+    [Tooltip("The sound made when the player loses")]
     public AudioClip LossSound;
-    [Tooltip("")]
+    [Tooltip("The sound the coin makes when collided with")]
     public AudioClip CoinSound;
     
     /// <summary>
-    /// 
+    /// set the variables to their starting values
+    /// get the object references
     /// </summary>
     void Start()
     {
+        //variables
         breakTimer = 3f;
         tutorial = false;
         HasMoved = false;
         direction = -1;
+        //object references 
         BreakSlider = GameObject.Find("BreakSlider");
         BreakSlider.GetComponent<Slider>().maxValue = breakTimer;
         BreakSlider.GetComponent<Slider>().value = breakTimer;
@@ -114,7 +117,8 @@ public class PlayerController : MonoBehaviour
     }
 
     /// <summary>
-    /// 
+    /// during the update the change direction function is called, then the line is drawn behind the player
+    /// then the method that checks the breaks is called
     /// </summary>
     void Update()
     {
@@ -138,15 +142,16 @@ public class PlayerController : MonoBehaviour
     }
 
     /// <summary>
-    /// 
+    /// this method controls the break speed by checking if the player is pressing shift or not
     /// </summary>
     private void CheckShift()
     {
+        //if the player is pressing shift and they still have break time left, the breaks are activated 
         if ((Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) && breakTimer > 0f && HasMoved)
         {
             breaks = true;
-            breakTimer -= Time.deltaTime;
-            BreakSlider.GetComponent<Slider>().value = breakTimer;
+            breakTimer -= Time.deltaTime; //tik down the breaktimer
+            BreakSlider.GetComponent<Slider>().value = breakTimer; //display new break timer
         }
         else
         {
@@ -155,20 +160,20 @@ public class PlayerController : MonoBehaviour
     }
 
     /// <summary>
-    /// 
+    /// this method controls the speed and direction the player is moving
     /// </summary>
     private void CheckMovement()
     {
         float localSpeed;
-        if ((breaks))//timer comes later
+        if ((breaks))//slow down
         {
             localSpeed = speed / 3f + 2f;
         }
-        else
+        else//normal speed
         {
             localSpeed = speed;
         }
-        if (HasMoved && !won)
+        if (HasMoved && !won)//move in the chosen direction
         {
             switch (direction)
             {
@@ -188,14 +193,15 @@ public class PlayerController : MonoBehaviour
             }
 
         }
-        else
+        else //stay still if all else fails
         {
             rb2d.velocity = new Vector3(0, 0, 0);
         }
     }
 
     /// <summary>
-    /// 
+    /// based on inputs (WASD) the direction is checekd and changed during fixed update  
+    /// every time the direction is changed the line behind the player has to be cut off and a new one must be made
     /// </summary>
     private void CheckDirection()
     {
@@ -229,7 +235,7 @@ public class PlayerController : MonoBehaviour
                 CreateLine();
                 AudioSource.PlayClipAtPoint(Turn, Camera.main.transform.position);
             }
-            else if((Input.GetKeyDown(KeyCode.Escape)))
+            else if((Input.GetKeyDown(KeyCode.Escape)))//leave the scene
             {
                 UnityEngine.SceneManagement.SceneManager.LoadScene("Selection Scene");
             }
@@ -237,49 +243,51 @@ public class PlayerController : MonoBehaviour
     }
 
     /// <summary>
-    /// 
+    /// this is called when the direction change
+    /// end the line currently being altered
+    /// create a new line and then give it to the script to alter
     /// </summary>
     private void CreateLine()
     {
-        LastLineEnd = transform.position;
-        GameObject G = Instantiate(Line, transform.position, Quaternion.identity);
-        NextCollide = G.GetComponent<Collider2D>();
+        LastLineEnd = transform.position;//set the end of the current line
+        GameObject G = Instantiate(Line, transform.position, Quaternion.identity); //make a new line
+        NextCollide = G.GetComponent<Collider2D>();//give the collider to the script in order ro stretch it
     }
 
     /// <summary>
-    /// 
+    /// this script is given the starting and ending point of a collider the shapes the collder between the two points
     /// </summary>
-    /// <param name="co"></param>
-    /// <param name="a"></param>
-    /// <param name="b"></param>
+    /// <param name="co">the collider of the newest line</param>
+    /// <param name="a">point 1</param>
+    /// <param name="b">point 2</param>
     private void FitColliderBetween(Collider2D co, Vector2 a, Vector2 b)
     {
         co.transform.position = a + (b - a) * .5f;
         float distance = Vector2.Distance(a, b);
 
-        if(a.x != b.x)
+        if(a.x != b.x)//increase it vertically
         {
             co.transform.localScale = new Vector2(distance + .5f,.5f);
         }
-        else
+        else//increase it horizontally
         {
             co.transform.localScale = new Vector2(.5f, distance + .5f);
         }
     }
 
     /// <summary>
-    /// 
+    /// this script handles triggers with other colliders and differentiates them by tags
     /// </summary>
-    /// <param name="collision"></param>
+    /// <param name="collision">the collider the object is interacting with</param>
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if((collision.gameObject.tag.Equals("Line") && collision != NextCollide))
+        if((collision.gameObject.tag.Equals("Line") && collision != NextCollide))//if you collide with a line that you aren't currently making
         {
             Invoke("DelayEnd", .75f);
             won = true;
             AudioSource.PlayClipAtPoint(LossSound, Camera.main.transform.position);
         }
-        if (collision.gameObject.tag.Equals("Coin"))
+        if (collision.gameObject.tag.Equals("Coin"))//collect the coin and then destroy it in the scene
         {
             Destroy(collision.gameObject);
             collision.gameObject.GetComponent<CoinBehaviour>().DeactivateCoin();
@@ -289,26 +297,26 @@ public class PlayerController : MonoBehaviour
     }
 
     /// <summary>
-    /// 
+    /// this script handles collisions with other colliders and differentiates them by tags
     /// </summary>
-    /// <param name="collision"></param>
+    /// <param name="collision">the collider the object is interacting with</param>
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag.Equals("Wall"))
+        if(collision.gameObject.tag.Equals("Wall"))//restart the game
         {
             Invoke("DelayEnd", .75f);
             won = true;
             AudioSource.PlayClipAtPoint(LossSound, Camera.main.transform.position);
 
         }
-        if (collision.gameObject.tag.Equals("Goal") && collision.gameObject.GetComponent<SpriteRenderer>().color == Color.blue)
+        if (collision.gameObject.tag.Equals("Goal") && collision.gameObject.GetComponent<SpriteRenderer>().color == Color.blue)//win the game
         {
             GameCon.ActivateWin();
             PlayerPrefs.SetInt(CurrentScene, 1);
             won = true;
             AudioSource.PlayClipAtPoint(WinSound, Camera.main.transform.position);
         }
-        else
+        else//if you hit anything else it's a loss
         {
             Invoke("DelayEnd", .75f);
             won = true;
@@ -317,7 +325,7 @@ public class PlayerController : MonoBehaviour
     }
 
     /// <summary>
-    /// 
+    /// called to change the scene and leave the current level
     /// </summary>
     private void DelayEnd()
     {
